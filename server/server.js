e9ce37f5a32fd328523dd00adf37adb02f46ee39
Chase Whitney <ChaseEWhitney@gmail.com>
2017-12-13 14:28:24 -0600

    first commit

--- server/server.js
+++ server/server.js
@@ -3,13 +3,6 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 var request = require('request');
-var API_KEY = process.env.API_KEY || require('../config.js').apiKey;
-
-// var fs = require('fs');
-// var allMatchData = [];
-
-// Route includes
-
 var port = process.env.PORT || 5001;
 
 // Body parser middleware
@@ -18,69 +11,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // Serve back static files
 app.use(express.static('./server/public'));
-
-// https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/RiotSchmick?api_key=<key>
-// /lol/summoner/v3/summoners/by-name/{summonerName}
-// /lol/match/v3/matches/{matchId} Get match by match ID.
-// /lol/match/v3/matchlists/by-account/{accountId} Get matchlist for games played on given account ID and platform ID and filtered using given filter parameters, if any.
-// /lol/match/v3/matchlists/by-account/{accountId}/recent Get matchlist for last 20 matches played on given account ID and platform ID.
-// /lol/match/v3/timelines/by-match/{matchId}
-app.get('/getSummonerID/:name', function(req, res){
-  var sumName = req.params.name;
-  var URL = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + sumName + '?api_key=' + API_KEY;
-  request(URL, function(err, response, body) {
-    if(err) {
-      console.log('error:', err);
-      res.sendStatus(500);
-    } else {
-      res.send(body);
-    }
-  });
-});
-
-app.get('/getMatches/:id', function(req, res){
-  var sumID = req.params.id;
-  var URL = 'https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/' + sumID + '?api_key=' + API_KEY;
-  request(URL, function(err, response, body) {
-    if(err) {
-      console.log('error:', err);
-      res.sendStatus(500);
-    } else {
-      res.send(body);
-    }
-  });
-});
-
-app.get('/getMatchData/:id', function(req, res){
-  var matchID = req.params.id;
-  var URL = 'https://na1.api.riotgames.com/lol/match/v3/matches/' + matchID + '?api_key=' + API_KEY;
-  request(URL, function(err, response, body) {
-    if(err) {
-      console.log('error:', err);
-      res.sendStatus(500);
-    } else {
-      allMatchData.push(JSON.parse(body));
-      res.send(body);
-    }
-  });
-});
-
-// app.get('/writeData', function(req, res){
-//   var dataObj = {data: allMatchData};
-//   var content = JSON.stringify(dataObj);
-//
-//   fs.writeFile("gameData.json", content, 'utf8', function (err) {
-//       if (err) {
-//           return console.log(err);
-//       }
-//
-//       console.log("The file was saved!");
-//       res.sendStatus(200);
-//   });
-//
-//
-//
-// });
 
 
 app.get('/', function(req, res) {
