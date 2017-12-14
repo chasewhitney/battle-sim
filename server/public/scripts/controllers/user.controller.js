38bd7f047cbedb1ffdc1ecd905abb1c4f64489b4
Chase Whitney <ChaseEWhitney@gmail.com>
2017-12-14 13:33:57 -0600

    hp fills out automatically but is editable

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -8,7 +8,8 @@ myApp.controller('UserController', function(UserService, $http, $location, $mdDi
   vm.lvlArray = buildNumberArray(1,50);
 
 
-function buildNumberArray(min, max){
+  function buildNumberArray(min, max){
+    // builds arrays of numbers for things like stats and levels
     tempArray = [];
     for (var i = min; i <= max; i++) {
       tempArray.push(i);
@@ -16,8 +17,29 @@ function buildNumberArray(min, max){
     return tempArray;
   }
 
+  vm.battle = function(){
+    console.log('in battle');
+  };
+
+  vm.hpInputPlaceholder = function(character){
+    if(!isNaN(character.lvl * (10+(character.con-17)) + 30)) {
+      if (!character.hpSet) {
+        character.hp = character.lvl * (10+(character.con-17)) + 30;
+        character.hpSet = true;
+      }
+      return character.lvl * (10+(character.con-17)) + 30;
+    }
+    else {
+      return "---";
+    }
+  };
+
+
   vm.test = function(){
     console.log('in test');
-    console.log(vm.testPlayer);
+    console.log('Player:', vm.testPlayer);
+    console.log('Enemy', vm.testEnemy);
   };
-});
+
+
+}); // end controller
