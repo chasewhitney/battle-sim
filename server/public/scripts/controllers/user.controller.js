16bc5aa229d965a7631cff99983e0ffc4d4386a9
Chase Whitney <ChaseEWhitney@gmail.com>
2017-12-13 18:37:25 -0600

    stat and level selection functional

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -2,6 +2,22 @@ myApp.controller('UserController', function(UserService, $http, $location, $mdDi
   console.log('UserController created');
   var vm = this;
   vm.userService = UserService;
+  vm.testPlayer = {};
+  vm.testEnemy = {};
+  vm.statArray = buildNumberArray(17,25);
+  vm.lvlArray = buildNumberArray(1,50);
 
 
+function buildNumberArray(min, max){
+    tempArray = [];
+    for (var i = min; i <= max; i++) {
+      tempArray.push(i);
+    }
+    return tempArray;
+  }
+
+  vm.test = function(){
+    console.log('in test');
+    console.log(vm.testPlayer);
+  };
 });
