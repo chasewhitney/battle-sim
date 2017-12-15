f58191d10bce1ed5ddf1c294ac7b5696d80eef87
Chase Whitney <ChaseEWhitney@gmail.com>
2017-12-14 23:25:59 -0600

    encounters can be simulated

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -2,8 +2,8 @@ myApp.controller('UserController', function(UserService, $http, $location, $mdDi
   console.log('UserController created');
   var vm = this;
   vm.userService = UserService;
-  vm.testPlayer = {};
-  vm.testEnemy = {};
+  vm.testPlayer = {name: "Player"};
+  vm.testEnemy = {name: "Enemy"};
   vm.statArray = buildNumberArray(17,25);
   vm.lvlArray = buildNumberArray(1,50);
 
@@ -17,15 +17,30 @@ myApp.controller('UserController', function(UserService, $http, $location, $mdDi
     return tempArray;
   }
 
-  function determineOrder(player, enemy){
-    // Determines who acts first
+  function beginBattle(player, enemy){
+    // Determines who acts first and begins battle
     playerAction(player, enemy);
+  }
+
+  function rollForHit(attacker, defender){
+    // Determines if attacks hits
+    for (var i = 0; i < attacker.numberOfAttacks; i++) {
+        var attackRoll = Math.floor(Math.random()*100+1+attacker.lvl+attacker.dex-17);
+        if (attackRoll >= defender.ac * 5) {
+          var damageRoll = Math.floor(Math.random()*8+1) + attacker.lvl/2;
+          defender.hp -= damageRoll;
+          console.log(attacker.name + '\'s attack hits for ' + damageRoll + ' damage!');
+        }
+        else {
+          console.log(attacker.name + '\'s attack goes wide!');
+
+        }
+    }
   }
 
   function playerAction(player, enemy){
     // Player turn in battle
-    enemy.hp -= 5;
-    console.log('Player hits Enemy for 5 damage!');
+    rollForHit(player, enemy);
     if(player.hp > 0 && enemy.hp > 0){
       enemyAction(player, enemy);
     }
@@ -36,8 +51,8 @@ myApp.controller('UserController', function(UserService, $http, $location, $mdDi
 
   function enemyAction(player, enemy){
     // Enemy turn in battle
-    player.hp -= 5;
-    console.log('Enemy hits Player for 5 damage!');
+    rollForHit(enemy, player);
+
     if(player.hp > 0 && enemy.hp > 0){
       playerAction(player, enemy);
     }
@@ -61,8 +76,9 @@ myApp.controller('UserController', function(UserService, $http, $location, $mdDi
 
   vm.battle = function(player, enemy){
     console.log('in battle');
+    // checkStats - make sure everything was entered
     vm.battleLog = [];
-    determineOrder(player, enemy);
+    beginBattle(player, enemy);
 
 
   };
